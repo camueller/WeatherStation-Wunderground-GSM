689713018720e6461f5a3e8dbf623540ae3b5566
Axel Müller <axel@mueller-family.de>
2016-01-16 12:59:00 +0100

    calculating wind speed every second

--- arduino/WindSpeed.ino
+++ arduino/WindSpeed.ino
@@ -1,14 +1,12 @@
 /*
- This Arduino sketch controls a personal weather station and uses a GSM shield to upload data to wunderground.com.
- For updates check https://github.com/camueller/GSM-GPRS-GPS-Shield
+ This Arduino sketch controls a personal weather station and use an ethernet shield to upload data to wunderground.com.
+ Details about the weather station can be found at https://github.com/camueller/WeatherStation-Wunderground-GSM.
  */
 
 //////////////
 // Variables
 //////////////
 
-// calculate wind speed across multiple seconds; otherwise speed can only be a multiple of 2.4 kph
-const int WIND_SPEED_CALC_INTERVAL_SECONDS = 3;
 const long WIND_SPEED_AVG_INTERVAL_SECONDS = 120;
 const long WIND_SPEED_GUST_INTERVAL_SECONDS = 600;
 volatile uint16_t windSpeedSignalCount;
@@ -38,6 +36,7 @@ void initWindSpeedSignalCount() {
   windSpeedSignalCount = 0;
 }
 
+
 //////////////
 // Loop
 //////////////
@@ -45,60 +44,61 @@ void initWindSpeedSignalCount() {
 void loopWindSpeed(unsigned long now)
 {
   // calculate wind speed  
-  if(now - windCalcIntervalBegin > 1000 * WIND_SPEED_CALC_INTERVAL_SECONDS || windCalcIntervalBegin > now) {
-    windSpeed = windSpeedSignalCount * 2.4 / WIND_SPEED_CALC_INTERVAL_SECONDS;
+  float intervalSeconds = (now - windCalcIntervalBegin) / 1000.0;
+  windSpeed = windSpeedSignalCount * 2.4 / intervalSeconds;
 #ifdef DEBUG_WS  
-    Serial.print(F("windSpeedSignalCount="));
-    Serial.println(windSpeedSignalCount);
+  Serial.print(F("windSpeedSignalCount="));
+  Serial.print(windSpeedSignalCount);
+  Serial.print(F(" intervalSeconds="));
+  Serial.println(intervalSeconds);
 #endif
 #ifdef INFO_WS
-    Serial.print(F("wind speed "));
-    Serial.print(windSpeed);
-    Serial.println(F(" kph"));
+  Serial.print(F("wind speed "));
+  Serial.print(windSpeed);
+  Serial.println(F(" kph"));
 #endif  
-    windSpeedSignalCount=0;  
-    windCalcIntervalBegin = now;
+  windSpeedSignalCount=0;  
+  windCalcIntervalBegin = now;
     
-    // calculate average
-    windSpeedAvgValues++;
-    windSpeedAvgValueSum += windSpeed;
+  // calculate average
+  windSpeedAvgValues++;
+  windSpeedAvgValueSum += windSpeed;
   
-    if(now - windSpeedAvgIntervalBegin > WIND_SPEED_AVG_INTERVAL_SECONDS * 1000) {
-      windSpeedAvg = windSpeedAvgValueSum / windSpeedAvgValues;
+  if(now - windSpeedAvgIntervalBegin > WIND_SPEED_AVG_INTERVAL_SECONDS * 1000 || windCalcIntervalBegin > now) {
+    windSpeedAvg = windSpeedAvgValueSum / windSpeedAvgValues;
 
 #ifdef DEBUG_WS  
-      Serial.print(F("windSpeedAvgValueSum="));
-      Serial.print(windSpeedAvgValueSum);
-      Serial.print(F(" windSpeedAvgValues="));
-      Serial.println(windSpeedAvgValues);
+    Serial.print(F("windSpeedAvgValueSum="));
+    Serial.print(windSpeedAvgValueSum);
+    Serial.print(F(" windSpeedAvgValues="));
+    Serial.println(windSpeedAvgValues);
 #endif
 #ifdef INFO_WS
-      Serial.print(F("Wind avg("));
-      Serial.print(WIND_SPEED_AVG_INTERVAL_SECONDS);
-      Serial.print(F("s): "));
-      Serial.print(windSpeedAvg);
-      Serial.println(F(" kph"));
+    Serial.print(F("Wind avg("));
+    Serial.print(WIND_SPEED_AVG_INTERVAL_SECONDS);
+    Serial.print(F("s): "));
+    Serial.print(windSpeedAvg);
+    Serial.println(F(" kph"));
 #endif      
-      windSpeedAvgIntervalBegin = now;
-      windSpeedAvgValues = 0;
-      windSpeedAvgValueSum = 0;
-    }
+    windSpeedAvgIntervalBegin = now;
+    windSpeedAvgValues = 0;
+    windSpeedAvgValueSum = 0;
+  }
     
-    if(now - windSpeedGustIntervalBegin > WIND_SPEED_GUST_INTERVAL_SECONDS * 1000 || windSpeedGustIntervalBegin > now) {
+  if(now - windSpeedGustIntervalBegin > WIND_SPEED_GUST_INTERVAL_SECONDS * 1000 || windSpeedGustIntervalBegin > now) {
 #ifdef INFO_WS
-      Serial.print(F("Wind gusts ("));
-      Serial.print(WIND_SPEED_GUST_INTERVAL_SECONDS);
-      Serial.print(F("s): "));
-      Serial.print(windSpeedGust);
-      Serial.println(F(" kph"));
+    Serial.print(F("Wind gusts ("));
+    Serial.print(WIND_SPEED_GUST_INTERVAL_SECONDS);
+    Serial.print(F("s): "));
+    Serial.print(windSpeedGust);
+    Serial.println(F(" kph"));
 #endif      
-      windSpeedGustIntervalBegin = now;
-      windSpeedGust = 0.0;
-    }
-    if(windSpeed > windSpeedGust) {
-      windSpeedGust = windSpeed;
-      windGustDirection = windDirection;
-    }
+    windSpeedGustIntervalBegin = now;
+    windSpeedGust = 0.0;
+  }
+  if(windSpeed > windSpeedGust) {
+    windSpeedGust = windSpeed;
+    windGustDirection = windDirection;
   }
 }
 
